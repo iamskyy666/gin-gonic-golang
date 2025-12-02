c57f50f3d853f35459e5e2fa8d8453cff423619e
Soumadip <39thdecember@gmail.com>
2025-12-02 23:28:37 +0530

    Add GET,POST endpoints ☑️

--- main.go
+++ main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-  "log"
-  "net/http"
+	"log"
+	"net/http"
 
-  "github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
@@ -17,6 +17,41 @@ func main() {
     c.JSON(http.StatusOK, gin.H{
       "message": "pong",
 	  "status":http.StatusOK,
+    })
+  })
+
+  // Define a GET endpoint with params
+  r.GET("/me/:id", func(c *gin.Context) {
+    id:=c.Param("id")
+    // Return JSON response
+    c.JSON(http.StatusOK, gin.H{
+      "user_id":id,
+    })
+  })
+
+   // Define a simple POST endpoint
+  r.POST("/me", func(c *gin.Context) {
+
+    type MeRequest struct{
+      Email string `json:"email" binding:"required"`
+      Password string `json:"password"`
+    }
+    var meReq MeRequest
+
+    err:=c.BindJSON(&meReq)
+    if err!=nil{
+      // Return JSON response
+    c.JSON(http.StatusBadRequest, gin.H{
+   "error":err.Error(),
+   "status_code":http.StatusBadRequest,
+    })
+    return
+    }
+
+    // Return JSON response
+    c.JSON(http.StatusOK, gin.H{
+   "email":meReq.Email,
+   "password":meReq.Password,
     })
   })
 
